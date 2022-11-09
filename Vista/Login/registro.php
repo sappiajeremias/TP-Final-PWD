0c1b379590e377923c1884c08b51d220fd9eabb1
BraianCenturion2001 <braiankrayan@hotmail.com>
2022-11-09 13:39:43 -0300

    forms login y registro cambios visuales

--- Vista/Login/registro.php
+++ Vista/Login/registro.php
@@ -2,24 +2,24 @@
 $Titulo = "Registrarse";
 include_once '../Estructura/cabecera.php';
 ?>
-<div class="container p-4 mt-5 border border-3 rounded-2 bg-light" style="width: 350px;">
+<div class="container p-4 mt-5 border border-dark border-3 rounded-2 bg-primary bg-opacity-10" style="width: 350px;">
     <h5 class="text-center"><i class="fa-solid fa-user-plus mx-2"></i>Registro</h5>
     <hr>
     <!-- INICIO FORMULARIO DE REGISTRO -->
     <form action="#" name="registro" id="registro" method="post" accept-charset="utf-8">
         <div class="form-group mb-3">
             <label for="usmail" class="form-label">Correo: </label>
-            <input type="email" class="form-control" id="usmail" name="usmail" autocomplete="off">
+            <input type="email" class="form-control border border-dark border-2" id="usmail" name="usmail" autocomplete="off">
         </div>
         <div class="form-group mb-3">
             <label for="usnombre" class="form-label">Nombre de Usuario: </label>
-            <input type="text" class="form-control" id="usnombre" name="usnombre" autocomplete="off">
+            <input type="text" class="form-control border border-dark border-2" id="usnombre" name="usnombre" autocomplete="off">
         </div>
         <div class="form-group mb-3">
             <label for="uspass" class="form-label">Contraseña: </label>
-            <input type="password" class="form-control" id="uspass" name="uspass" autocomplete="off">
+            <input type="password" class="form-control border border-dark border-2" id="uspass" name="uspass" autocomplete="off">
         </div>
-        <button type="submit" class="btn btn-outline-success">Registrarse</button>
+        <button type="submit" class="btn btn-success">Registrarse</button>
     </form>
     <!-- FIN FORMULARIO DE REGISTRO -->
 </div>
