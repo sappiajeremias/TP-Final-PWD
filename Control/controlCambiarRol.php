05caecf209cfc573d41d966d6417823eb7546aec
BraianCenturion2001 <braiankrayan@hotmail.com>
2022-11-11 11:15:52 -0300

    nuevo mensaje controlCambiarRol

--- Control/controlCambiarRol.php
+++ Control/controlCambiarRol.php
@@ -10,20 +10,25 @@ $sesion = new Session();
 
 $idRolBuscar = $sesion->buscarIdRol($idNuevoRol); // VERIFICAMOS SI EL ID ENVIADO COINCIDE CON ALGÚN ROL EXISTENTE
 
+
 if($idRolBuscar<>null){ // SI EXISTE EL ROL
-    $_SESSION['rolactivoid'] = $idRolBuscar; // SETEAMOS EL NUEVO ID ROL
-    switch($idRolBuscar){ // SETEAMOS LA DESCRIPCION DEL NUEVO ROL
-        case '1':
-            $_SESSION['rolactivodescripcion'] = 'admin';
-            break;
-        case '2':
-            $_SESSION['rolactivodescripcion'] = 'deposito';
-            break;
-        case '3':
-            $_SESSION['rolactivodescripcion'] = 'cliente';
-            break;
-       }
-    $mensaje = "Se cambió de rol exitosamente!";
+    if($_SESSION['rolactivoid']<>$idRolBuscar){ // SI EL ROL ES DISTINTO AL YA SETEADO HACEMOS EL CAMBIO
+        $_SESSION['rolactivoid'] = $idRolBuscar; // SETEAMOS EL NUEVO ID ROL
+        switch($idRolBuscar){ // SETEAMOS LA DESCRIPCION DEL NUEVO ROL
+            case '1':
+                $_SESSION['rolactivodescripcion'] = 'admin';
+                break;
+            case '2':
+                $_SESSION['rolactivodescripcion'] = 'deposito';
+                break;
+            case '3':
+                $_SESSION['rolactivodescripcion'] = 'cliente';
+                break;
+        }
+        $mensaje = "Se cambió exitosamente al rol: ".strtoupper($_SESSION['rolactivodescripcion']);
+    } else {
+        $mensaje = "Usted ya tiene activo el rol: ".strtoupper($_SESSION['rolactivodescripcion']);
+    }
 } else {
     $mensaje = "No se pudo actualizar el rol!";
 }
