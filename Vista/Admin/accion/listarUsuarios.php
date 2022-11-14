2e10e262d93b40a7ff0ba9f460847a809c1e8665
RoniBroilo <ronniebroilo@gmail.com>
2022-11-14 16:06:33 -0300

    Método para listarUsuarios de Admin

--- Vista/Admin/accion/listarUsuarios.php
+++ Vista/Admin/accion/listarUsuarios.php
@@ -1,19 +1,19 @@
-<?php 
+<?php
 include_once "../../../configuracion.php";
 $data = data_submitted();
-$objControl = new abmUsuario();
-$list = $objControl->buscar($data);
-$arreglo_salida =  array();
-foreach ($list as $elem ){
-    
-    $nuevoElem['idusuario'] = $elem->getIdMenu();
-    $nuevoElem['usnombre']=$elem->getUsNombre();
-    $nuevoElem['usmail']=$elem->getUsMail();
-    $nuevoElem['usdeshabilitado']=$elem->getUsDeshabilitado();
-    array_push($arreglo_salida,$nuevoElem);
-    
-}
-//verEstructura($arreglo_salida);
-echo json_encode($arreglo_salida);
+$arregloUsuarios =  array();
 
-?>
+$objControl = new abmUsuario();
+$listaUsuarios = $objControl->buscar($data);
+$arregloUsuarios =  array();
+foreach ($listaUsuarios as $elem) {
+
+    $nuevoElem['idusuario'] = $elem->getID();
+    $nuevoElem['usnombre'] = $elem->getUsNombre();
+    $nuevoElem['usmail'] = $elem->getUsMail();
+    $nuevoElem['usdeshabilitado'] = $elem->getUsDeshabilitado();
+    array_push($arregloUsuarios, $nuevoElem);
+}
+
+//verEstructura($arregloUsuarios);
+echo json_encode($arregloUsuarios);
