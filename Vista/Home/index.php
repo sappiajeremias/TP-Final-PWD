b88c0749853a315babea11ad617151aff20b09a2
BraianCenturion2001 <braiankrayan@hotmail.com>
2022-11-26 20:02:26 -0300

    cartelito mensaje index cambiado por un toast

--- Vista/Home/index.php
+++ Vista/Home/index.php
@@ -4,40 +4,47 @@ include_once '../Estructura/cabecera.php';
 $datos = data_submitted();
 if (!empty($datos['mensaje'])) {
 ?>
-    <div class='container pt-3 text-center'>
-        <div class='alert alert-warning alert-dismissible fade show' role='alert'>
-            <i class='fa-solid fa-check mx-2'></i> <?php echo $datos['mensaje'] ?>
-            <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+    <div class="position-fixed p-2 bottom-0 end-0">
+        <div class="toast show text-bg-primary">
+            <div class="toast-header">
+                <i class="fa-solid fa-envelope me-2"></i>
+                <strong class="me-auto">Mensaje Sistema</strong>
+                <small>hace 2 minutos</small>
+                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">
+                <span><?php echo $datos['mensaje'] ?></span>
+            </div>
         </div>
     </div>
 <?php } ?>
 <div class="container">
-<div class="container p-2">
-    <div class="alert alert-info" role="alert">
-        Orden Estados de Compra
-    </div>
-</div>
-<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel" style="width: 500px;">
-    <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="5000">
-            <img src="../img/carousel1.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item" data-bs-interval="2000">
-            <img src="../img/carousel2.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-            <img src="../img/carousel3.png" class="d-block w-100" alt="...">
+    <div class="container p-2">
+        <div class="alert alert-info" role="alert">
+            Orden Estados de Compra
         </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
-</div>
+    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel" style="width: 500px;">
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="5000">
+                <img src="../img/carousel1.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item" data-bs-interval="2000">
+                <img src="../img/carousel2.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="../img/carousel3.png" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
 </div>
 
 
