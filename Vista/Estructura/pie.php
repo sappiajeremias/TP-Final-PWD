d14a3388d9ead698c7dffe9c2c6d44f4b18e2073
BraianCenturion2001 <braiankrayan@hotmail.com>
2022-11-26 22:20:33 -0300

    enlaces github agregados al pie

--- Vista/Estructura/pie.php
+++ Vista/Estructura/pie.php
@@ -1,23 +1,28 @@
+<!-- INICIO PIE -->
 <section class="mt-4" style="user-select: none;">
     <footer class="text-center text-white" style="background-color: #0a4275;">
-        <div class="container p-4 pb-0">
+        <div class="container p-4 pb-2">
             <div class="row text-center">
-            <div class="col">
-                <h5>Laureano Luna</h5>
-                <p>FAI-3543</p>
-            </div> |
-            <div class="col">
-                <h5>Jeremias Sappia</h5>
-                <p>FAI-2532</p>
-            </div> |
-            <div class="col">
-                <h5>Braian Centurión</h5>
-                <p>FAI-3001</p>
-            </div> |
-            <div class="col">
-                <h5>Roni Broilo</h5>
-                <p>FAI-1107</p>
-            </div>
+                <div class="col">
+                    <a href="https://github.com/LaureanoLuna" class="text-white"><h5><i class="fa-brands fa-github"></i></h5></a>
+                    <h5>Laureano Luna</h5>
+                    <p>FAI - 3543</p>
+                </div> |
+                <div class="col">
+                    <a href="https://github.com/sappiajeremias" class="text-white"><h5><i class="fa-brands fa-github"></i></h5></a>
+                    <h5>Jeremias Sappia</h5>
+                    <p>FAI - 2532</p>
+                </div> |
+                <div class="col">
+                    <a href="https://github.com/BraianCenturion2001" class="text-white"><h5><i class="fa-brands fa-github"></i></h5></a>
+                    <h5>Braian Centurión</h5>
+                    <p>FAI - 3001</p>
+                </div> |
+                <div class="col">
+                    <a href="https://github.com/RoniBroilo" class="text-white"><h5><i class="fa-brands fa-github"></i></h5></a>
+                    <h5>Roni Broilo</h5>
+                    <p>FAI - 1107</p>
+                </div>
             </div>
         </div>
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
