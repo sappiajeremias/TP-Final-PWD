509e62e3cc450a61dbaabcdc31bafdcd9e64a3ee
BraianCenturion2001 <braiankrayan@hotmail.com>
2022-11-30 21:10:24 -0300

    listadoproductos se ven las imagenes y no rompen el footer

--- Vista/Home/productos.php
+++ Vista/Home/productos.php
@@ -1,13 +1,12 @@
 <?php
 $Titulo = "Productos";
 include_once '../Estructura/cabecera.php';
-
 ?>
-<div class="container p-2" id="contenedorProductos">
-        <div class="row" id="filaProductos" >
-               
-</div>
-<script src="../../Utiles/js/listarProductos.js"></script>
-<?php 
 
-include_once '../Estructura/pie.php'; ?>
+<div class="container p-2" id="contenedorProductos">
+        <div class="row" id="filaProductos"></div>
+</div>
+
+<script src="../../Utiles/js/listarProductos.js"></script>
+
+<?php include_once '../Estructura/pie.php' ?>
